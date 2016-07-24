ed0e98b0f96ce269470f30e1fd96ea2a9e014535
Sewagegn <sintayehu244@yahoo.com>
2016-07-24 11:45:05 -0600

    update

--- Fullstack/Restaurant/main.js
+++ Fullstack/Restaurant/main.js
@@ -1,56 +1,56 @@
-function Drink(name,description,price,ingredients) {
-	this.name=name;
-	this.description=description;
-	this.price=price;
-	this.ingredients=[];
-}
-function Plate(name,description,price,ingredients) {
-	this.name=name;
-	this.description=description;
-	this.price=price;
-	this.ingredients=[];
-}
-// function OrderMenu(item) {
-//    this.item=[];
+// function Drink(name,description,price,ingredients) {
+// 	this.name=name;
+// 	this.description=description;
+// 	this.price=price;
+// 	this.ingredients=[];
 // }
-// OrderMenu.prototype.totalprice=function() {
-// 	var sum=0;
-// 	for(var i=0;i<item.length;i++) {
-// 		var sum+=this.item[i];
-// 		return sum;
-// 	}
+// function Plate(name,description,price,ingredients) {
+// 	this.name=name;
+// 	this.description=description;
+// 	this.price=price;
+// 	this.ingredients=[];
 // }
+// // function OrderMenu(item) {
+// //    this.item=[];
+// // }
+// // OrderMenu.prototype.totalprice=function() {
+// // 	var sum=0;
+// // 	for(var i=0;i<item.length;i++) {
+// // 		var sum+=this.item[i];
+// // 		return sum;
+// // 	}
+// // }
 
-//Drinks we carry
-var wine= new Drink('white wine','alcohol',10)
-wine.ingredients.push('water,15% alcohol');
-var water=new Drink('mountain spring','FDA approved',6)
-water.ingredients.push('ca+,k+,fe++');
+// //Drinks we carry
+// var wine= new Drink('white wine','alcohol',10)
+// wine.ingredients.push('water,15% alcohol');
+// var water=new Drink('mountain spring','FDA approved',6)
+// water.ingredients.push('ca+,k+,fe++');
 
-//our Food menu
-// var burger=new Plate('cheese burger','house special',8)
-// burger.ingredients.push('cheese,luttce,tomato,french bagget');
-// var burrito=new Plate('burrito','mexican burrito',10) ;
-// burrito.ingredients.push('white rice,chicken,lettuce');
+// //our Food menu
+// // var burger=new Plate('cheese burger','house special',8)
+// // burger.ingredients.push('cheese,luttce,tomato,french bagget');
+// // var burrito=new Plate('burrito','mexican burrito',10) ;
+// // burrito.ingredients.push('white rice,chicken,lettuce');
 
-//preference
-
-
-
-///main menu
-// function MainMenu(food,drink) 
-// {
-// 	this.food=[];
-// 	this.drink=[];
-// }
-// MainMenu.prototype.foodMenu=function() {
-// 	this.food.push(Plate.name);
-// }
-// MainMenu.prototype.drinkMenu=function() {
-// 	this.drink.push(Drink.name);
-// }
+// //preference
 
 
-//checkup
+
+// ///main menu
+// // function MainMenu(food,drink) 
+// // {
+// // 	this.food=[];
+// // 	this.drink=[];
+// // }
+// // MainMenu.prototype.foodMenu=function() {
+// // 	this.food.push(Plate.name);
+// // }
+// // MainMenu.prototype.drinkMenu=function() {
+// // 	this.drink.push(Drink.name);
+// // }
+
+
+// //checkup
 
 
